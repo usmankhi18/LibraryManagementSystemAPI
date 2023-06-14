9d512ee33662303079af90c9afbd37db04dd818b
Muhammad Usman Khan <usman.khi18@hotmail.com>
2023-06-15 00:53:58 +0500

    RedisCache Enabled

--- BusinessLogic/Implementation/StudentService.cs
+++ BusinessLogic/Implementation/StudentService.cs
@@ -1,6 +1,9 @@
-﻿using BusinessLogic.Interfaces;
+﻿using BusinessLogic.ApplicationCache;
+using BusinessLogic.Interfaces;
+using Global.AppSettings;
 using IRepository;
 using POCO.Models;
+using StackExchange.Redis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,21 +15,30 @@ namespace BusinessLogic.Implementation
     public class StudentService : IStudentService
     {
         private readonly IStudentRepository studentRepository;
+        private readonly StudentCache studentCache;
 
-        public StudentService(IStudentRepository studentRepository)
+        public StudentService(IStudentRepository studentRepository,StudentCache cache)
         {
             this.studentRepository = studentRepository;
+            this.studentCache = cache;
         }
 
         public bool DeleteStudent(Student student)
         {
-            return studentRepository.DeleteStudent(student);
+            bool isDelete = studentRepository.DeleteStudent(student);
+            studentCache.ClearCache(AppSettingKeys.RedisKey);
+            return isDelete;
         }
 
         public List<Student> GetAllStudents()
         {
-            // Call the repository method to get returning students
-            return studentRepository.GetAllStudents();
+            List<Student> students = studentCache.GetStudentsFromCache(AppSettingKeys.RedisKey);
+            if (students == null)
+            {
+                // Call the repository method to get returning students
+                return studentRepository.GetAllStudents();
+            }
+            return students;
         }
 
         public List<Student> GetStudents(Student student)
@@ -36,12 +48,16 @@ namespace BusinessLogic.Implementation
 
         public bool InsertStudents(Student student)
         {
-            return studentRepository.InsertStudents(student);
+            bool isInsert = studentRepository.InsertStudents(student);
+            studentCache.ClearCache(AppSettingKeys.RedisKey);
+            return isInsert;
         }
 
         public bool UpdateStudent(Student student)
         {
-            return studentRepository.UpdateStudent(student);
+            bool isUpdate = studentRepository.UpdateStudent(student);
+            studentCache.ClearCache(AppSettingKeys.RedisKey);
+            return isUpdate;
         }
     }
 }
